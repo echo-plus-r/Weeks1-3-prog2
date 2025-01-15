50709d24e96d42c92ff59ab74280663c84da00e1
echo-plus-r <tostesi@shernet.sheridancollege.ca>
2025-01-15 10:57:37 -0500

    looked at animation curves and such

---
+++ Assets/scripts/lerpdemo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lerpdemo : MonoBehaviour
+{
+    public AnimationCurve curve;
+    [Range(0f, 1f)]
+    public float t;
+
+    public Transform start;
+    public Transform end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 pos = new Vector2(Mathf.Lerp(-4, 5, t), 0);
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+    }
+}
