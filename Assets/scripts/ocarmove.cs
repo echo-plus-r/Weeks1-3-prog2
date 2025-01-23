cdda0e236d5dcbbbf1b81314bdaca8492244d29e
DESKTOP-2933QV0\simon <tostesi@sheridancollege.ca>
2025-01-23 11:56:00 -0500

    reworked the code in the other car script (ocarmove) to fix an error i made during planning. i treated the movement of the other car like i'd be adding and removing numbers from it's position instead of setting its position. the script now checks if "cx" is 1 or over, then sets it to 0. then adds to it if it's below 1. the right boolean, along with the if and else statements related to it. the animation curve was also changed slightly, making it more of a sin wave. if i understand trigonometry, this probably shouldn't really do anything. in short, the code is now far more simplistic and the other car now moves more smoothly.

--- Assets/scripts/ocarmove.cs
+++ Assets/scripts/ocarmove.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class ocarmove : MonoBehaviour
 {
     float cx;
-    Boolean right = true;
     public AnimationCurve OtherCurve;
     // Start is called before the first frame update
     void Start()
@@ -19,38 +18,20 @@ public class ocarmove : MonoBehaviour
     {
         Vector2 pos = transform.position;
 
-        // checks if the car is moving right (runs if "right" boolean is true)
-        if (right)
-        {
-            // checks if "cx" float (handles animation curve result) is 1 or above
-            if (cx >= 1)
-            {
-                // sets right boolean to false
-                right = false;
-            }
 
-            // if "cx" float is below 1, it adds a little to the "cx" float
-            else {
-                cx += 0.1f * Time.deltaTime;
-            }
+        // checks if "cx" float (handles animation curve result) is 1 or above
+        if (cx >= 1)
+        {
+            cx = 0;
         }
 
-        // runs if the car is moving left
+        // if "cx" float is below 1, it adds a little to the "cx" float
         else 
         {
-            // checks if "cx" (handles animation curve result) float is 0 or under
-            if (cx <= 0)
-            {
-                // sets the right boolean to true
-                right = true;
-            }
-
-            // if "cx" float is above 0, removes a small amount from "cx"
-            else
-            {
-                cx -= 0.1f * Time.deltaTime;
-            }
+            cx += 0.1f * Time.deltaTime;
         }
+
+        
 
         // temporary numbers, use screen point position as the lerp should be between the left edge of the screen and the right edge of the screen
         pos.x = Mathf.Lerp(0, 10, OtherCurve.Evaluate(cx));
